ed274c0e129f8b3738e7f927555d6eb82201b943
Joel S <jmsmith.coding@gmail.com>
2025-02-24 23:48:39 -0800

    updated vite.config

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: "./env",
   plugins: [react()],
   server: {
     port: 10000,
-    host: true
+    host: true,
   },
   preview: {
     port: 10000,
-    host: true
-  }
+    host: true,
+    allowedHosts: ["candidate-search-engine.onrender.com"],
+  },
 });
